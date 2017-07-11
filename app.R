36a9719f25aa435111eef6b379271013413fcb43
Aaron Ferrucci <aaron.ferrucci@gmail.com>
2017-07-10 19:58:11 -0700

    Working demo.

--- app.R
+++ app.R
@@ -1,5 +1,6 @@
 
 library(shiny)
+library(ggplot2)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -10,16 +11,16 @@ ui <- fluidPage(
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
-         sliderInput("bins",
-                     "Number of slices:",
+         sliderInput("rects",
+                     "Number of rectangles:",
                      min = 1,
-                     max = 50,
-                     value = 30)
+                     max = 64,
+                     value = 31)
       ),
       
       # Show a plot of the generated distribution
       mainPanel(
-         plotOutput("distPlot")
+         plotOutput("plot")
       )
    )
 )
@@ -27,14 +28,37 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
    
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+  output$plot <- renderPlot({
+    # plot object with a null data item
+    p <- ggplot(data = data.frame(x = 0), mapping = aes(x = x))
+     
+    # define the bounding functions
+    f1 <- function(x) {
+      return(sqrt(x))
+    }
+    f2 <- function(x) {
+      return(x^2)
+    }
+    n = input$rects
+    
+    # Compute the rectangle bounds and the approximating area
+    xmin <- 0
+    xmax <- 1
+    w <- (xmax - xmin) / n
+    x1 <- xmin + c(0:(n-1)) * w
+    x2 <- x1 + w
+    y1 <- f2(x1)
+    y2 <- f1(x1)
+    
+    rects <- data.frame(x1 = x1, x2 = x2, y1 = y1, y2 = y2, t=rep('a', n))
+    area <- sum((rects$x2 - rects$x1) * (rects$y2 - rects$y1))
+    
+    # Plot the functions
+    p2 <- p + scale_x_continuous(limits=c(0, 1.1)) + stat_function(fun=f1) + stat_function(fun=f2) +
+      ggtitle(bquote(Area~between~x^2~and~sqrt(x)~is~approximately~.(area))) + guides(fill=F)
+    # final plot with rectangles
+    p2 + geom_rect(data=rects, mapping=aes(xmin=x1, xmax=x2, ymin=y1, ymax=y2, fill=t), color="black", alpha=0.5)
+  })
 }
 
 # Run the application 
